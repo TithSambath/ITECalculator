5d65a412759c7040cd6e378dc6d078f944ffa6a0
TithSambath <dilytithsambath@gmail.com>
2019-10-30 08:26:02 +0700

    New update: - Add Instruction for convert number system - Add javadoc to each function/Method in NumberSystemConversion class.

--- src/com/ITE/NumberSystemConversion.java
+++ src/com/ITE/NumberSystemConversion.java
@@ -9,9 +9,28 @@ import java.util.function.BinaryOperator;
  */
 public class NumberSystemConversion {
 
-    private int decimal;
+    /*
+       Instruction to convert from one number system to another:
+            1.Think of the user input value as any number system
+              depend on each parameter of the function that its
+              use.
+            2.Convert all those number system by using
+               all this (Predefine function) function
+                Integer.parseInt();
+                Integer.toString();
+                Integer.toHex();
+                Integer.toBinaryString();
+                //This 4 function above take string as a parameter.//
+               also use this user-define function: binary2Decimal(int binary);
+               to convert all any number system to decimal then convert it to
+               what user number system want.
+     */
 
-
+    /**
+     * This binary2Decimal is use to convert from Binary system to Decimal system.
+     * @param binary take user input as a binary system.(only 0 and 1)
+     * @return value of decimal as string.
+     */
     String binary2Decimal (int binary){
         String binary_as_string = Integer.toString(binary);
         int Decimal_as_number = Integer.parseInt(binary_as_string,2);
@@ -19,6 +38,11 @@ public class NumberSystemConversion {
         return Decimal_as_string;
     }
 
+    /**
+     * This binary2Octal is use to convert from Binary system to Octal system.
+     * @param binary take user input as a binary system.(Accept only 0 and 1)
+     * @return value of Octal as string.
+     */
     String binary2Octal (int binary){
         String Decimal_as_string = binary2Decimal(binary);
         int Decimal_as_number = Integer.parseInt(Decimal_as_string,10);
@@ -26,6 +50,11 @@ public class NumberSystemConversion {
         return Octal_as_string;
     }
 
+    /**
+     * This binary2Octal is use to convert from Binary system to Hexadecimal system.
+     * @param binary take user input as a binary system.(Accept only 0 and 1)
+     * @return value of Hexadecimal as string.
+     */
     String binary2Hexadecimal (int binary){
         String Decimal_as_string = binary2Decimal(binary);
         int Decimal_as_number = Integer.parseInt(Decimal_as_string);
@@ -33,12 +62,22 @@ public class NumberSystemConversion {
         return Hexadecimal_as_string;
     }
 
+    /**
+     * This Octal2Decimal is use to convert from octal system to decimal system.
+     * @param Octal take user input as a octal sytem.
+     * @return value of Decimal as string.
+     */
     String Octal2Decimal (int Octal){
-        String octal = Integer.toString(Octal);
-        String Decimal = Integer.toString(Integer.parseInt(octal,8));
-        return Decimal;
+        String octal_as_string = Integer.toString(Octal);
+        String Decimal_as_string = Integer.toString(Integer.parseInt(octal_as_string,8));
+        return Decimal_as_string;
     }
 
+    /**
+     * This Octal2Binary is use to convert from octal system to Binary system.
+     * @param octal take user input value as an octal system.
+     * @return value of Binary as string.
+     */
     String Octal2Binary (int octal) {
         String Decimal_as_string = Octal2Decimal(octal);
         int Decimal_as_number = Integer.parseInt(Decimal_as_string);
@@ -46,7 +85,12 @@ public class NumberSystemConversion {
         return Binary_as_string;
     }
 
-    String Octal2Hex (int octal) {
+    /**
+     * This Octal2Hexadecimal is use to convert from octal system to Binary system.
+     * @param octal take user input value as an octal system.
+     * @return value of Hexadecimal as string.
+     */
+    String Octal2Hexadecimal (int octal) {
         String Decimal_as_string = Octal2Decimal(octal);
         int Decimal_as_number = Integer.parseInt(Decimal_as_string);
         String Hexadecimal_as_string = Integer.toHexString(Decimal_as_number);
